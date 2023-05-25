b286b45d3fe4ead8aed52432831ca2d5ca7c829a
Hanqing Qi <billy.qi03@gmail.com>
2023-05-25 15:05:28 -0400

    Format code and add comment

--- Infrared/Soccer_Game/Server_Py.py
+++ Infrared/Soccer_Game/Server_Py.py
@@ -1,5 +1,9 @@
 # echo-server.py
-
+'''
+Author: Hanqing Qi
+Date: 05/25/2023 
+Description: This is the server that listen to ESP32.
+'''
 import socket
 import time
 HOST = "192.168.0.41"  # Standard loopback interface address (localhost)
@@ -9,20 +13,19 @@ PORT = 80  # Port to listen on (non-privileged ports are > 1023)
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
     s.listen()
-    
     try:
         while True:
             conn, addr = s.accept()
             print(f"Connected by {addr}")
             with conn: 
-                print("Waiting for data")
-
+                print("Waiting for data...")
                 while True:       
                     data = conn.recv(1)
                     if not data:
+                        print("No data")
                         break
                     if data:
-                        print(len(data), data[0])
+                        print("Data: ", len(data), data[0])
             time.sleep(1)
     except:
         print("Closing connection")
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Blimps_Team
+
+This repository contains the code for the blimps competition happens on Nov 11.
