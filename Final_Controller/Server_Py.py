f96b9bdea5af42f68c98e59d26da8581a6144068
Hanqing Qi <billy.qi03@gmail.com>
2023-06-01 17:55:08 -0400

    👾 Socket and Infrared interferes

--- Final_Controller/Server_Py.py
+++ Final_Controller/Server_Py.py
@@ -7,7 +7,7 @@ Description: This is the server that listen to ESP32.
 import socket
 import time
 HOST = "192.168.0.41"  # Standard loopback interface address (localhost)
-PORT = 80  # Port to listen on (non-privileged ports are > 1023)
+PORT = 122  # Port to listen on (non-privileged ports are > 1023)
 Flag = 0  # Flag to control the car
 
 IP_ADDRESS = '192.168.0.204'
@@ -26,7 +26,7 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             with conn: 
                 print("Waiting for data...")
                 while True:
-                    data = conn.recv(1)
+                    data = conn.recv(2)
                     # Make sure the buffer is empty before reading
                     if not data:
                         print("No data")                                
@@ -56,8 +56,9 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
                             time.sleep(0.2)
                             command = 'CMD_MOTOR#00#00#00#00\n'
                             # cars.send(command.encode('utf-8'))
-            time.sleep(0.3)
-    except:
+                # time.sleep(0.3)
+    except Exception as e:
+        print(e)
         command = 'CMD_MOTOR#00#00#00#00\n'
         # cars.send(command.encode('utf-8'))
         print("Closing connection")
--- Final_Receiver/Infrared_Remote.py
+++ Final_Receiver/Infrared_Remote.py
@@ -1,26 +1,31 @@
-'''
+"""
 Author: Hanqing Qi
 Date: 2023-05-31 15:55:24
 LastEditors: Hanqing Qi
-LastEditTime: 2023-06-01 16:14:43
+LastEditTime: 2023-06-01 16:19:49
 FilePath: /Blimps_Team/Final_Receiver/Infrared_Remote.py
 Description: This is the infrared reciever file that will be used to recieve the IR signal from the remote
-'''
+"""
 # Import the library for the IR reciever
 from irrecvdata import irGetCMD
 import utime
+
 # Import the library for the network connection
-import network 
-import socket 
+import network
+import socket
 import time
 
 # Variables for the network connection
 ssidRouter = "AIRLab-BigLab"
 passwordRouter = "Airlabrocks2022"
 host = "192.168.0.41"
-port = 80
-wlan=None 
-s=None
+port = 122
+wlan = None
+s = None
+
+# Temp Counter
+Total = 0
+Catch = 0
 
 # Send Flag
 # NOTE: The flag is a two digit number with the following meaning:
@@ -29,13 +34,17 @@ s=None
 #       1: target         | 1: target
 #       2: noise          | 2: noise
 sendFlag = 00
+# Signal
+Sig = 0x807F08F7
 
-'''
+"""
 description: This function is used to connect the ESP32 to the router
 param {*} ssid
 param {*} passwd
 return {*} True if the connection is successful
-'''
+"""
+
+
 def connectWifi(ssid, passwd):
     global wlan
     wlan = network.WLAN(network.STA_IF)
@@ -43,53 +52,65 @@ def connectWifi(ssid, passwd):
     wlan.disconnect()
     wlan.connect(ssid, passwd)
 
-    while wlan.ifconfig()[0] == '0.0.0.0':
+    while wlan.ifconfig()[0] == "0.0.0.0":
         time.sleep(1)
     return True
 
+
 print("==============Start of program==============")
-recvPinL = irGetCMD(15) # Left reciever has PIN 15
+recvPinL = irGetCMD(15)  # Left reciever has PIN 15
 print("Left eye online")
-recvPinR = irGetCMD(14) # Right reciever has PIN 14
+recvPinR = irGetCMD(14)  # Right reciever has PIN 14
 print("Right eye online")
 try:
     # Connect to the router
     connectWifi(ssidRouter, passwordRouter)
     s = socket.socket()
     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    s.connect((host,port))
+    s.connect((host, port))
     print("TCP Connected to:", host, ":", port)
     # Start the reciever
     print("Listening")
     while True:
-        sendFlag = 00 # Reset the sendFlag
-        utime.sleep_ms(100) 
-        print("."), # This is just to show that the program is running
+        sendFlag = 00  # Reset the sendFlag
+        utime.sleep_ms(90)
+        print("."),  # This is just to show that the program is running
         irValueL = recvPinL.my_irread()
         irValueR = recvPinR.my_irread()
         if irValueL:
-            print("Left: ", irValueL)
+            print(irValueL)
+            if irValueL == hex(Sig):
+                sendFlag += 10
+                print("Target L")
+            else:
+                sendFlag += 20
+                print("Noise L")
         if irValueR:
-            print("Right: ", irValueR)
-            
-except Exception as e:
+            print(irValueR)
+            if irValueR == hex(Sig):
+                sendFlag += 1
+                print("Target R")
+            else:
+                sendFlag += 2
+                print("Noise R")
+        if irValueL and irValueR:
+            if irValueL == irValueR:
+                sendFlag = 11
+        if sendFlag != 0:
+            Total += 1
+            print("sendFlag: ", sendFlag)
+        if sendFlag == 11:
+            Catch += 1
+        # s.send(str(1))
+        utime.sleep_ms(110)
+
+# except Exception as e:
+except KeyboardInterrupt:
     print("TCP close, please reset!")
     if s:
         s.close()
     wlan.disconnect()
     wlan.active(False)
-    print(e)
+    print(Total, Catch, Catch / Total)
+    # print(e)
     pass
-
-        
-
-
-
-
-
-
-
-
-
-
-
