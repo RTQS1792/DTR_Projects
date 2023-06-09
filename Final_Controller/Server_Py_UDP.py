6d202566e654b1b523a84d33676fb7cbbeb9490c
Hanqing Qi <billy.qi03@gmail.com>
2023-06-09 19:26:20 -0400

    Dailu Final Commit

--- Final_Controller/Server_Py_UDP.py
+++ Final_Controller/Server_Py_UDP.py
@@ -7,6 +7,7 @@ FilePath: /Blimps_Team/Python/UDP_Example/UDP_Server.py
 Description: This is the UDP server that receives data from the receiver and control the vehicle
 '''
 import socket
+import time
 # Define the IP address and port
 ip_address = '192.168.0.41'  # or use '0.0.0.0' to listen on all available interfaces
 port = 8848  # Replace with your port
@@ -20,8 +21,8 @@ server_socket.bind(server_address)
 print(f"Server is listening on {ip_address}:{port}")
 
 CAR_IP = '192.168.0.204'
-# cars = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# cars.connect((CAR_IP, 5000))
+cars = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+cars.connect((CAR_IP, 5000))
 print('Cars connected')
 
 Blind_Flag = 0
@@ -34,26 +35,57 @@ while True:
     if len(data) == 2:
         Blind_Flag = 0
         if data[0] == 49 and data[1] == 48: # Left Target Right Blind
+            command = 'CMD_MOTOR#-1100#-1100#1100#1100\n'
+            cars.send(command.encode('utf-8'))
+            time.sleep(0.1)
             print("←")
         elif data[0] == 49 and data[1] == 50: # Left Target Right Noise
+            command = 'CMD_MOTOR#-1100#-1100#1100#1100\n'
+            cars.send(command.encode('utf-8'))
+            time.sleep(0.1)
             print("↖")
         elif data[0] == 50 and data[1] == 48: # Left Noise Right Blind
+            command = 'CMD_MOTOR#-1100#-1100#1100#1100\n'
+            cars.send(command.encode('utf-8'))
+            time.sleep(0.1)
             print("↖")
         elif data[0] == 50 and data[1] == 49: # Left Noise Right Target
+            command = 'CMD_MOTOR#1100#1100#-1100#-1100\n'
+            cars.send(command.encode('utf-8'))
+            time.sleep(0.1)
             print("↗")
         elif data[0] == 49 and data[1] == 49: # Target
+            command = 'CMD_MOTOR#1100#1100#1100#1100\n'
+            cars.send(command.encode('utf-8'))
+            time.sleep(0.1)
             print("↑")
         elif data[0] == 50 and data[1] == 50: # Left Noise Right Noise
+            command = 'CMD_MOTOR#1500#1500#-1500#-1500\n'
+            cars.send(command.encode('utf-8'))
+            time.sleep(0.1)
             print("↗")
     elif len(data) == 1:
         if data[0] == 48: # Left Blind Right Blind
             Blind_Flag += 1
             if Blind_Flag >= 10:
+                command = 'CMD_MOTOR#1100#1100#-1100#-1100\n'
+                cars.send(command.encode('utf-8'))
+                time.sleep(0.1)
                 print("X")
+            else:
+                command = 'CMD_MOTOR#0#0#0#0\n'
+                cars.send(command.encode('utf-8'))
+                time.sleep(0.1)
         elif data[0] == 49: # Left Blind Right Target
+            command = 'CMD_MOTOR#1100#1100#-1100#-1100\n'
+            cars.send(command.encode('utf-8'))
+            time.sleep(0.1)
             Blind_Flag = 0
             print("→")        
         elif data[0] == 50: # Left Blind Right Noise
+            command = 'CMD_MOTOR#1100#1100#-1100#-1100\n'
+            cars.send(command.encode('utf-8'))
+            time.sleep(0.1)
             Blind_Flag = 0
             print("↗")
         
