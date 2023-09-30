76ec58c6bd7a6e003943bff7137e8f0e88f114e6
Hanqing Qi <billy.qi03@gmail.com>
2023-09-30 17:16:50 -0400

    ✏️ ESPNOWControl Class (MMA V1.1.4)

--- ESP_NOW/Multi_Mac_Address_V1/Main.py
+++ ESP_NOW/Multi_Mac_Address_V1/Main.py
@@ -2,7 +2,25 @@
 Author       : Hanqing Qi
 Date         : 2023-09-29 16:25:39
 LastEditors  : Hanqing Qi
-LastEditTime : 2023-09-29 16:25:40
-FilePath     : /Multi_Mac_Address_V1/Main.py
+LastEditTime : 2023-09-30 17:13:01
+FilePath     : /DTR_Projects/ESP_NOW/Multi_Mac_Address_V1/Main.py
 Description  : Main for testing ESP-NOW Communication
 """
+
+from Serial_Sender import ESPNOWControl
+import time
+
+PORT = "/dev/cu.wchusbserial1140"
+LIST_OF_MAC_ADDRESS = [
+    "a3:2f:67:b2:45:89",
+    "7e:14:c2:9b:d3:56",
+    "b0:92:4c:a1:78:e3",
+    "5f:13:87:d0:69:72",
+    "4a:22:3b:e9:50:61",
+]
+SLAVE_INDEX = 0
+BRODCAST_CHANNEL = 1
+
+if __name__ == "__main__":
+    # Initialize the serial connection
+    esp_now = ESPNOWControl(PORT, LIST_OF_MAC_ADDRESS)
--- ESP_NOW/Multi_Mac_Address_V1/Serial_Sender.py
+++ ESP_NOW/Multi_Mac_Address_V1/Serial_Sender.py
@@ -1,81 +1,72 @@
 import serial
 import time
-import sys
 
-PORT = "/dev/cu.wchusbserial1140"
-MAC_ADDRESSES = [
-    "11:11:11:2b:11:11",
-    "22:22:22:22:22:22",
-    "33:33:33:33:33:33",
-    "44:44:44:44:44:11",
-    "54:4a:16:1a:1a:1a",
-]
-SLAVE_ID = 3
-BRODCAST_MODE = 1
+NULL_ADDRESS = ["00:00:00:00:00:00"]
 DELIMITER = "|"
 
+class ESPNOWControl:
+    def __init__(self, serial_port: str, mac_addresses: list = NULL_ADDRESS) -> None:
+        if(self._init_serial(serial_port)):
+            print("Serial connection established")
+        else:
+            raise Exception("Serial connection failed")
+        self._send_mac_addresses(mac_addresses)
+        print("ESP-NOW Control Initialized Successfully")
+        self.broadcast_mode = False
+        if mac_addresses == NULL_ADDRESS:
+            print("No MAC addresses provided, broadcast mode enabled")
+            self.broadcast_mode = True
 
-# Class for control input
-class ControlInput:
-    def __init__(self, *args, slave_id: int = 0, broadcast_mode: int = 0) -> None:
-        """
-        @description: The constructor of ControlInput class
-        @param       {*} self: -
-        @param       {array} args: 13 parameters for ControlInput 
-        @param       {int} slave_id: The id of the slave to control, default to 0
-        @param       {int} broadcast_mode: The mode of broadcast, default to 0, no broadcast
-        @return      {None}
-        NOTE: broadcast_mode can override slave_id, if broadcast is enabled, slave_id will be ignored
-        """
-        if len(args) != 13:
-            raise ValueError("Expected 13 parameters for ControlInput")
-        self.params = args
-        self.slave_id = slave_id
-        self.broadcast_mode = broadcast_mode
-
-    def __str__(self) -> str:
-        """
-        @description: Get the string representation of ControlInput class
-        @param       {*} self: -
-        @return      {str} The string representation
-        """
-        return (
-            "<"
-            + DELIMITER.join(map(str, self.params))
-            + DELIMITER
-            + str(self.slave_id)
-            + DELIMITER
-            + str(self.broadcast_mode)
-            + ">"
-        )
-
-
-def espnow_init()->serial.Serial:
-    """
-    @description: Initialize the ESP-NOW connection
-    @return      {serial.Serial} The serial connection
-    """
-    try:
-        ser = serial.Serial(PORT, 115200)
-        return ser
-    except serial.SerialException as e:
-        print(f"Failed to connect to port {PORT}. Error: {e}")
-        sys.exit(1)
-
-
-def send_mac_addresses(ser):
-    print("Sending MAC addresses...")
-    while True:
-        mac_data = "${}#{}$".format(len(MAC_ADDRESSES), "#".join(MAC_ADDRESSES))
-        ser.write(mac_data.encode())
+    def _init_serial(self, serial_port: str) -> bool:
         try:
-            incoming = ser.readline().decode(errors="ignore").strip()
-            if incoming == ("Number of addresses: " + str(len(MAC_ADDRESSES))):
-                print("MAC addresses sent successfully!")
-                break
+            self.serial = serial.Serial(serial_port, 115200)
+            print(f"Connected to port {serial_port}")
+            # Empty the buffer
+            while self.serial.in_waiting:
+                self.serial.readline().decode(errors="ignore").strip()
+            time.sleep(1)
+            return True
+        except serial.SerialException as e:
+            print(f"Failed to connect to port {serial_port}. Error: {e}")
+            return False
+        
+    def _send_mac_addresses(self, mac_addresses: list) -> None:
+        print("Sending MAC addresses...")
+        while True:
+            mac_data = "${}#{}$".format(len(mac_addresses), "#".join(mac_addresses))
+            self.serial.write(mac_data.encode())
+            try:
+                incoming = self.serial.readline().decode(errors="ignore").strip()
+                if incoming == ("Number of addresses: " + str(len(mac_addresses))):
+                    print("MAC addresses sent successfully!")
+                    break
+            except UnicodeDecodeError:
+                print("Received malformed data!")
+            time.sleep(0.5)
+
+    def ESPNOW_send(self, control_params: list, brodcast_channel:int, slaveindex: int) -> bool:
+        if len(control_params) != 13:
+            raise ValueError("Expected 13 control parameters but got {}".format(len(control_params)))
+        raw_massage = control_params
+        if self.broadcast_mode or slaveindex == -1: # Broadcast mode
+            raw_massage.append(brodcast_channel)
+            raw_massage.append(-1)
+        else:
+            raw_massage.append(-1)
+            raw_massage.append(slaveindex)
+        # Format the message
+        message = str("<" + DELIMITER.join(map(str, raw_massage)) + ">")
+        self.serial.write(message.encode())
+        try:
+            incoming = self.serial.readline().decode(errors="ignore").strip()
+            print("Received Data: " + incoming)
         except UnicodeDecodeError:
             print("Received malformed data!")
-        time.sleep(1)
+
+    def close(self) -> None:
+        if self.serial.is_open:
+            self.serial.close()
+            print("Serial connection closed.")
 
 
 def esp_now_send(ser, input):
