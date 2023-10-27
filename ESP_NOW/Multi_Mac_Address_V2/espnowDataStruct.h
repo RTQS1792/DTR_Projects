4b506d7a14a772247891a2c5fc0cd738785b4aa0
Hanqing Qi <billy.qi03@gmail.com>
2023-10-27 14:33:47 -0400

    💾 Save `espnowDataStruct.h`
    Complete data struct while adding comments.

--- ESP_NOW/Multi_Mac_Address_V2/espnowDataStruct.h
+++ ESP_NOW/Multi_Mac_Address_V2/espnowDataStruct.h
@@ -2,7 +2,7 @@
  * @Author       : Hanqing Qi
  * @Date         : 2023-10-26 18:11:25
  * @LastEditors  : Hanqing Qi
- * @LastEditTime : 2023-10-26 18:26:50
+ * @LastEditTime : 2023-10-27 14:31:42
  * @FilePath     : /Multi_Mac_Address_V2/espnowDataStruct.h
  * @Description  : The data structure to hold the information in espnow communication.
  */
@@ -14,10 +14,17 @@
 
 #define MAX_PARAMS 16
 
-typedef struct esp_now_data_struct
-{
-    int paramCount;  // This int indicates the number of parameters in use
-    int params[MAX_PARAMS];  // Array to hold 16 int parameters
+typedef struct esp_now_data_struct {
+    int paramCount;         // This int indicates the number of parameters in use
+    int params[MAX_PARAMS]; // Array to hold 16 int parameters
+    int flag;               // Flag that indicates which type of data is being sent
+    /*
+     * NOTE: Below is the list of flags and their corresponding data types
+     * 0: regular control data
+     * 1: BNP & barometer feedback
+     * 2: nicla feedback
+     * 3: wall sensor feedback
+     */
 } esp_now_data_struct;
 
 #endif
