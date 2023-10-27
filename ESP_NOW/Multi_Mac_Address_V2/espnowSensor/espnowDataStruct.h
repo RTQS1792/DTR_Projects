df38dd53b927240b4f8693fd472567ceb0dfd47c
Hanqing Qi <billy.qi03@gmail.com>
2023-10-27 16:00:46 -0400

    ✅ Passed `espnowSensor.ino` & 🦾 Add Documentation `espnowDataStruct.h`
    Espnow sensor code passed initial test, add documentation for header file.

---
+++ ESP_NOW/Multi_Mac_Address_V2/espnowSensor/espnowDataStruct.h
@@ -0,0 +1,62 @@
+/*
+ * @Author       : Hanqing Qi
+ * @Date         : 2023-10-26 18:11:25
+ * @LastEditors  : Hanqing Qi
+ * @LastEditTime : 2023-10-27 15:56:57
+ * @FilePath     : /Multi_Mac_Address_V2/espnowSensor/espnowDataStruct.h
+ * @Description  : The data structure to hold the information in espnow
+ * communication.
+ */
+
+#ifndef _ESP_NOW_DATA_STRUCT_H_
+#define _ESP_NOW_DATA_STRUCT_H_
+
+#include <Arduino.h>
+
+#define MAX_PARAMS 16
+
+typedef struct esp_now_data_struct {
+  int paramCount;         // This int indicates the number of parameters in use
+  int params[MAX_PARAMS]; // Array to hold 16 int parameters
+  int flag; // Flag that indicates which type of data is being sent
+            /*
+             * NOTE: Below is the list of flags and their corresponding data types
+             * 0: regular control data
+             * 1: BNP & barometer feedback
+             * 2: nicla feedback
+             * 3: wall sensor feedback
+             */
+} esp_now_data_struct;
+
+#endif
+
+/**
+ * @description: Print the data in the espnowDataStruct.
+ * @param {esp_now_data_struct} data: The data to be printed.
+ * @return {*} None
+ */
+void printEspnowData(const esp_now_data_struct &data) {
+  // Prints the flag of the data.
+  switch (data.flag) {
+  case 0:
+    Serial.print("Control data: ");
+    break;
+  case 1:
+    Serial.print("BNP & baro feedback: ");
+    break;
+  case 2:
+    Serial.print("Nicla feedback: ");
+    break;
+  case 3:
+    Serial.print("Wall sensor feedback: ");
+    break;
+  default:
+    Serial.print("Unknown flag: ");
+    break;
+  }
+  // Print all parameters in the data.
+  for (int i = 0; i < data.paramCount; i++) {
+    Serial.print(data.params[i]);
+    Serial.print(" ");
+  }
+}
--- ESP_NOW/Multi_Mac_Address_V2/espnowSensor/espnowSensor.ino
+++ ESP_NOW/Multi_Mac_Address_V2/espnowSensor/espnowSensor.ino
@@ -2,79 +2,65 @@
  * @Author       : Hanqing Qi
  * @Date         : 2023-10-26 18:32:19
  * @LastEditors  : Hanqing Qi
- * @LastEditTime : 2023-10-27 14:58:24
+ * @LastEditTime : 2023-10-27 15:23:48
  * @FilePath     : /Multi_Mac_Address_V2/espnowSensor/espnowSensor.ino
- * @Description  : 
+ * @Description  :
  */
 
 #include <Arduino.h>
 #include <esp_now.h>
 #include <WiFi.h>
 
-#define CHANNEL 1 // Choose a channel between 1 and 13
+#include "espnowDataStruct.h" // Include your data structure definition
 
-#include "esp_now_data_struct.h"  // Include your data structure definition
+uint8_t robotAddress[] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00}; // MAC address of the receiver
+esp_now_data_struct dataToSend;                                // Create a struct to hold the data you will send
+esp_now_peer_info_t peerInfo;
 
-uint8_t robotAddress[] = {0x24, 0x0A, 0xC4, 0x81, 0xA8, 0xAC};  // MAC address of the receiver
-esp_now_data_struct dataToSend;  // Create a struct to hold the data you will send
-
-void debugLED(uint8_t pin, uint8_t errorType) {
-    pinMode(pin, OUTPUT);  // Ensure the pin is set as OUTPUT
-    if (errorType == 0) {  // Error during setup
-        while (true) {
-            digitalWrite(pin, LOW); // NOTE: The led control is reversed, LOW will enable the LED and HIGH will disable it
-        }
-    } 
-    else if (errorType == 1) {  // Error during loop
-        for (int i = 0; i < 3; i++) {  // Blink 3 times to indicate error
-            digitalWrite(pin, LOW);
-            delay(200);  // Faster blink
-            digitalWrite(pin, HIGH);
-            delay(200);
-        }
-    }
-}
-
-void setup() {
+void setup()
+{
     pinMode(LED_BUILTIN, OUTPUT); // Enable the LED_BUILTIN pin as an debug LED
+    digitalWrite(LED_BUILTIN, LOW);
     Serial.begin(115200);
     WiFi.mode(WIFI_STA);
-    if (esp_now_init() != ESP_OK) {
+    Serial.print("ESP Sensor MAC Address:  ");
+    Serial.println(WiFi.macAddress());
+    if (esp_now_init() != ESP_OK)
+    {
         Serial.println("Error initializing ESP-NOW");
-        debugLED(LED_BUILTIN, 0); // Error Initializing ESP-NOW
-        return;
-    }
-    esp_now_peer_info_t peerInfo;
-    memcpy(peerInfo.peer_addr, robotAddress, 6);
-    peerInfo.channel = CHANNEL;
-    peerInfo.encrypt = false;
-    if (esp_now_add_peer(&peerInfo) != ESP_OK){
-        Serial.println("Failed to add peer");
-        debugLED(LED_BUILTIN, 0); // Error Adding Peer
         return;
     }
     esp_now_register_send_cb(onDataSent);
+    memcpy(peerInfo.peer_addr, robotAddress, 6);
+    peerInfo.encrypt = false;
+    if (esp_now_add_peer(&peerInfo) != ESP_OK)
+    {
+        Serial.println("Failed to add peer");
+        return;
+    }
+  digitalWrite(LED_BUILTIN, HIGH);
 }
 
-void loop() {
+void loop()
+{
     esp_now_data_struct data;
     data.paramCount = 1; // Only need one parameter for wall sensor feedback
     data.params[0] = 99; // TODO: Change this to the actual wall sensor feedback
-    data.flag = 3; // Flag for wall sensor feedback
-    lastSentData = data;  // Store the data before sending
-    esp_now_send(robotAddress, (uint8_t *) &data, sizeof(data));
-    delay(2000);  // Send data every 2 seconds
+    data.flag = 3;       // Flag for wall sensor feedback
+    dataToSend = data; // Store the data before sending
+    esp_now_send(robotAddress, (uint8_t *)&data, sizeof(data));
 }
 
-
-void onDataSent(const uint8_t *mac_addr, esp_now_send_status_t status, const uint8_t *data, int len) {
+void onDataSent(const uint8_t *mac_addr, esp_now_send_status_t status)
+{
+    printEspnowData(dataToSend); // Print the data that was sent
     Serial.print("Sent to: ");
-    for (int i = 0; i < 6; i++) {
+    for (int i = 0; i < 6; i++)
+    {
         Serial.print(mac_addr[i], HEX);
-        if (i < 5) Serial.print(":");
+        if (i < 5)
+            Serial.print(":");
     }
     Serial.print(", Status: ");
     Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Success" : "Fail    ");
-
-    printEspnowData(lastSentData);  // Print the data that was sent
 }
